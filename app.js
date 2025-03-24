e4dbeb1a4f64aa0134f63701b0669ba94a495931
fabiomolignoni <fabio.molignoni@studenti.unitn.it>
2025-03-24 11:47:42 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/', function (req, res) {
+	res.send('Hello, this is a test with a comment\n');
+});
+app.listen(8080, function () {
+	console.log('This is a log after the listen');
+});
+module.exports = app;
