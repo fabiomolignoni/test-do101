06f497ac5064fc13d29a80d87db4ca1646f05d3d
Student User <student@workstation.lab.example.com>
2025-03-24 07:03:28 -0400

    modifica

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
-	res.send('Hello, this is a test with a comment\n');
+	res.send('Hello, this is a test with a comment\n seconda riga');
 });
 app.listen(8080, function () {
 	console.log('This is a log after the listen');
