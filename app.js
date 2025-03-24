d3ff936139151290d205f473e8f3ac871c2f5738
Student User <student@workstation.lab.example.com>
2025-03-24 07:58:27 -0400

    Fix typo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
-	res.send('Hello, this is a test with a comment\n seconda riga \n terza rigay');
+	res.send('Hello, this is a test with a comment\n seconda riga \n terza riga');
 });
 app.listen(8080, function () {
 	console.log('This is a log after the listen');
