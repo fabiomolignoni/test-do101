5b1ffde4bd04657be2d6c093017fcebc126a72ce
fabiomolignoni <fabio.molignoni@studenti.unitn.it>
2025-03-25 10:17:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
-	res.send('Hello all!, this is a test with a comment and I also add a test for the webhook! :)');
+	res.send('Hello each and all!, this is a test with a comment and I also add a test for the webhook! :)');
 });
 app.listen(8080, function () {
 	console.log('This is a log after the listen');
